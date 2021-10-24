a5509bda2747754257d354132d9cbb9c565ecf86
Maxime Blade <gabel.maxime.blade@gmail.com>
2021-10-25 00:47:42 +0200

    Fixed Broken Imports

--- src/build/buildDomComponent.js
+++ src/build/buildDomComponent.js
@@ -1,8 +1,8 @@
 let fs = require("flowfs/mkdirp");
 let rollup = require("rollup");
 let svelte = require("rollup-plugin-svelte");
-let resolve = require("rollup-plugin-node-resolve");
-let commonjs = require("rollup-plugin-commonjs");
+let resolve = require("@rollup/plugin-node-resolve");
+let commonjs = require("@rollup/plugin-commonjs");
 let cssOnly = require("rollup-plugin-css-only");
 let json = require("@rollup/plugin-json");
 let terser = require("terser");
@@ -11,99 +11,101 @@ let babel = require("./babel");
 
 let pathStartRe = /([A-Z]:|\/)/;
 
-module.exports = async function(path, name, config) {
-	let prod = config.env === "prod";
-	let dev = !prod;
-	let {dynamicImports} = config;
-	let transpile = prod && !dynamicImports;
-	let minify = prod;
-	
-	let inputOptions = {
-		input: path,
-		
-		plugins: [
-			svelte({
-				extensions: [".svelte", ".html"],
-				
-				preprocess: {
-					style: sass,
-				},
-				
-				onwarn() {},
-				
-				compilerOptions: {
-					hydratable: true,
-					dev,
-				},
-			}),
-	
-			resolve({
-				browser: true,
-			}),
-			
-			commonjs(),
-			
-			json(),
-			
-			cssOnly({
-				output(c) {
-					// do nothing - CSS is provided by the SSR component
-				},
-			}),
-		],
-	};
-	
-	/*
+module.exports = async function (path, name, config) {
+    let prod = config.env === "prod";
+    let dev = !prod;
+    let { dynamicImports } = config;
+    let transpile = prod && !dynamicImports;
+    let minify = prod;
+
+    let inputOptions = {
+        input: path,
+
+        plugins: [
+            svelte({
+                extensions: [".svelte", ".html"],
+
+                preprocess: {
+                    style: sass,
+                },
+
+                onwarn() {},
+
+                compilerOptions: {
+                    hydratable: true,
+                    dev,
+                },
+            }),
+
+            resolve({
+                browser: true,
+            }),
+
+            commonjs(),
+
+            json(),
+
+            cssOnly({
+                output(c) {
+                    // do nothing - CSS is provided by the SSR component
+                },
+            }),
+        ],
+    };
+
+    /*
 	if we're transpiling, do CJS instead of IIFE.  this is because babel with
 	useBuiltIns: "usage" (required for converting async/await) adds CJS
 	requires for core-js.  The babel code takes a CJS module, babels it, then
 	rolls it up into an IIFE (so with transpilation it ends up being rolled up
 	twice)
 	*/
-	
-	let outputOptions = {
-		name,
-		format: dynamicImports ? "es" : (transpile ? "cjs" : "iife"),
-	};
-	
-	let bundle = await rollup.rollup(inputOptions);
-	
-	let {output} = await bundle.generate(outputOptions);
-	
-	if (dynamicImports) {
-		await bundle.write({
-			dir: fs(path).parent.reparent(config.dir, config.buildDir).path,
-		});
-	}
-	
-	let js = output[0];
-	
-	if (transpile) {
-		js = await babel(path, name, js.code);
-	}
-	
-	if (minify) {
-		js = terser.minify(js.code);
-	}
-	
-	return {
-		js,
-		
-		watchFiles: bundle.watchFiles.map(function(path) {
-			/*
+
+    let outputOptions = {
+        name,
+        format: dynamicImports ? "es" : transpile ? "cjs" : "iife",
+    };
+
+    let bundle = await rollup.rollup(inputOptions);
+
+    let { output } = await bundle.generate(outputOptions);
+
+    if (dynamicImports) {
+        await bundle.write({
+            dir: fs(path).parent.reparent(config.dir, config.buildDir).path,
+        });
+    }
+
+    let js = output[0];
+
+    if (transpile) {
+        js = await babel(path, name, js.code);
+    }
+
+    if (minify) {
+        js = terser.minify(js.code);
+    }
+
+    return {
+        js,
+
+        watchFiles: bundle.watchFiles
+            .map(function (path) {
+                /*
 			some paths have markers from rollup plugins - strip these for watching
 			
 			some are also not absolute; these are also internal to rollup and can
 			be stripped
 			*/
-			
-			let start = path.match(pathStartRe);
-			
-			if (start) {
-				return path.substr(start.index);
-			} else {
-				return false;
-			}
-		}).filter(Boolean),
-	};
-}
+
+                let start = path.match(pathStartRe);
+
+                if (start) {
+                    return path.substr(start.index);
+                } else {
+                    return false;
+                }
+            })
+            .filter(Boolean),
+    };
+};
--- src/build/buildSsrComponent.js
+++ src/build/buildSsrComponent.js
@@ -1,86 +1,86 @@
 let fs = require("flowfs/mkdirp");
 let rollup = require("rollup");
 let svelte = require("rollup-plugin-svelte");
-let resolve = require("rollup-plugin-node-resolve");
-let commonjs = require("rollup-plugin-commonjs");
+let resolve = require("@rollup/plugin-node-resolve");
+let commonjs = require("@rollup/plugin-commonjs");
 let cssOnly = require("rollup-plugin-css-only");
 let json = require("@rollup/plugin-json");
 let sass = require("./sass");
 
-module.exports = async function(path, config) {
-	let dev = config.env === "dev";
-	let css;
-	
-	let inputOptions = {
-		input: path,
-		
-		external(id, parentId, resolved) {
-			if (!config.svelteDirs) {
-				return false;
-			}
-			
-			let file;
-			
-			if (id.match(/^\./)) {
-				file = fs(parentId).sibling(id);
-			} else {
-				file = fs(config.root).child("node_modules").child(id);
-			}
-			
-			return config.svelteDirs.every(function(dir) {
-				return file.path !== dir.path && !file.within(dir);
-			});
-		},
-		
-		plugins: [
-			svelte({
-				extensions: [".svelte", ".html"],
-				
-				onwarn() {},
-			
-				preprocess: {
-					style: sass,
-				},
-				
-				compilerOptions: {
-					hydratable: true,
-					dev,
-					generate: "ssr",
-				},
-			}),
-	
-			resolve({
-				browser: true,
-			}),
-			
-			commonjs(),
-			
-			json(),
-			
-			cssOnly({
-				output(c) {
-					css = c;
-				},
-			}),
-		],
-		
-		onwarn(warning, next) {
-			if (warning.code !== "UNUSED_EXTERNAL_IMPORT") {
-				next(warning);
-			}
-		},
-	};
-	
-	let outputOptions = {
-		format: "cjs",
-	};
-	
-	let bundle = await rollup.rollup(inputOptions);
-	
-	let {output} = await bundle.generate(outputOptions);
-	
-	return {
-		component: output[0].code,
-		css,
-	};
-}
+module.exports = async function (path, config) {
+    let dev = config.env === "dev";
+    let css;
+
+    let inputOptions = {
+        input: path,
+
+        external(id, parentId, resolved) {
+            if (!config.svelteDirs) {
+                return false;
+            }
+
+            let file;
+
+            if (id.match(/^\./)) {
+                file = fs(parentId).sibling(id);
+            } else {
+                file = fs(config.root).child("node_modules").child(id);
+            }
+
+            return config.svelteDirs.every(function (dir) {
+                return file.path !== dir.path && !file.within(dir);
+            });
+        },
+
+        plugins: [
+            svelte({
+                extensions: [".svelte", ".html"],
+
+                onwarn() {},
+
+                preprocess: {
+                    style: sass,
+                },
+
+                compilerOptions: {
+                    hydratable: true,
+                    dev,
+                    generate: "ssr",
+                },
+            }),
+
+            resolve({
+                browser: true,
+            }),
+
+            commonjs(),
+
+            json(),
+
+            cssOnly({
+                output(c) {
+                    css = c;
+                },
+            }),
+        ],
+
+        onwarn(warning, next) {
+            if (warning.code !== "UNUSED_EXTERNAL_IMPORT") {
+                next(warning);
+            }
+        },
+    };
+
+    let outputOptions = {
+        format: "cjs",
+    };
+
+    let bundle = await rollup.rollup(inputOptions);
+
+    let { output } = await bundle.generate(outputOptions);
+
+    return {
+        component: output[0].code,
+        css,
+    };
+};
